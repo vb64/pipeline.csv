61676f1244967f05ba9853147378e8435b36560e
vb64 <vit.sar68@gmail.com>
2023-10-18 16:15:02 +0400

    refactor with decorator

--- pipeline_csv/csvfile/defect.py
+++ pipeline_csv/csvfile/defect.py
@@ -38,28 +38,27 @@ class Defect:
         """Return True if seam placed defect."""
         return self.code in self.row.atseam_dict()
 
-    @property
-    def at_weld_seam(self):
-        """Return True if defect located at weld/seam."""
-        return self.is_at_weld or self.is_at_seam
+    def _with_mp(prop):  # pylint: disable=no-self-argument
+        """Return decorator for property.
+
+        https://stackoverflow.com/questions/1263451/python-decorators-in-classes
+        """
+        def wrapper(self):
+            """Return None if defect does not have maximum depth point, or defect is located on a seam/weld."""
+            if self.is_at_weld or self.is_at_seam or (not self.row.mpoint_dist):
+                return None
+            return prop(self)  # pylint: disable=not-callable
+
+        return wrapper
 
     @property
-    def no_mp(self):
-        """Return True if defect does not have maximum depth point."""
-        return not self.row.mpoint_dist
-
-    @property
+    @_with_mp
     def mp_left_weld(self):
         """Return distance (mm) from maximum depth point to upstream weld."""
-        if self.at_weld_seam or self.no_mp:
-            return None
-
         return self.row.mpoint_dist - self.pipe.dist
 
     @property
+    @_with_mp
     def mp_right_weld(self):
         """Return distance (mm) from maximum depth point to downstream weld."""
-        if self.at_weld_seam or self.no_mp:
-            return None
-
         return self.pipe.dist + self.pipe.length - self.row.mpoint_dist
