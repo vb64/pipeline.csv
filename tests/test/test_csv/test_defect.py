8754f8c2a7a34355ebfa34c996e8e627bdddbf60
vb64 <vit.sar68@gmail.com>
2023-10-23 13:42:02 +0400

    refactor

--- tests/test/test_csv/test_defect.py
+++ tests/test/test_csv/test_defect.py
@@ -18,29 +18,39 @@ class TestDefect(TestCsv):
         Tube.diam = 700
         self.pipe = Tube(Row.as_weld(10), Stream(), '1')
 
-    def test_props(self):  # pylint: disable=too-many-statements
-        """Check defekt properties."""
-        from pipeline_csv import DefektSide, TypeHorWeld
-        from pipeline_csv.orientation import Orientation
+    def make_defect(self, dist, length, orient1, orient2, mp_orient, mp_dist):
+        """Make new defect."""
+        from pipeline_csv import DefektSide
         from pipeline_csv.oegiv import TypeDefekt, Row
         from pipeline_csv.csvfile.defect import Defect
 
-        orient1 = Orientation(9, 10)
-        orient2 = Orientation(5, 10)
-        mp_orient = Orientation(11, 0)
-
-        row = Row.as_defekt(
-          10, TypeDefekt.CORROZ, DefektSide.INSIDE, '10', '10', '15', orient1, orient2, mp_orient, 11, 'test defect'
+        return Defect(
+          Row.as_defekt(
+            dist, TypeDefekt.CORROZ, DefektSide.INSIDE, str(length), '10', '15',
+            orient1, orient2,
+            mp_orient, mp_dist, ''
+          ),
+          self.pipe
         )
-        assert row.orient_td == "9,10"
-        assert row.orient_bd == "5,10"
-        assert row.dist == 10
+
+    def test_props(self):
+        """Check defekt properties."""
+        from pipeline_csv import TypeHorWeld
+        from pipeline_csv.orientation import Orientation
+        from pipeline_csv.oegiv import Row
 
         assert self.pipe.dist == 10
         self.pipe.length = 12000
-        self.pipe.thick = 120
 
-        defect = Defect(row, self.pipe)
+        defect = self.make_defect(
+          10, 10,
+          Orientation(9, 10), Orientation(5, 10),
+          Orientation(11, 0), 11
+        )
+
+        assert defect.row.orient_td == "9,10"
+        assert defect.row.orient_bd == "5,10"
+        assert defect.row.dist == 10
         assert 'Коррозия at ' in str(defect)
         assert defect.code == 0
         assert defect.is_metal_loss
