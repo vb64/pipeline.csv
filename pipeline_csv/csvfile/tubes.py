a0937bc262a8ab69230eeec388a304cf42a4911f
Vitaly Bogomolov <vit.sar68@gmail.com>
2024-12-09 14:22:25 +0400

    restore min_diam

--- pipeline_csv/csvfile/tubes.py
+++ pipeline_csv/csvfile/tubes.py
@@ -135,12 +135,12 @@ class Tube:  # pylint: disable=too-many-instance-attributes
     @property
     def min_diam(self):
         """Return optional minimal pipe diameter as string (mm)."""
-        return self.row.depth_min
+        return self.row.depth_max
 
     @min_diam.setter
     def min_diam(self, value):
         """Set optional minimal pipe diameter as string (mm)."""
-        self.row.depth_min = value
+        self.row.depth_max = value
 
     @property
     def number(self):
