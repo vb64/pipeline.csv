bec061ca8d9972a30eecbd05ed1d4e0a6b037243
Vitaly Bogomolov <vit.sar68@gmail.com>
2024-12-14 09:44:00 +0400

    Fix build dir creation

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,8 +13,4 @@ def path_setup():
 @pytest.fixture(scope="session", autouse=True)
 def session_setup(request):
     """Auto session resource fixture."""
-    build_path = 'build'
-    if not os.path.exists(build_path):
-        os.mkdir(build_path)
-
     path_setup()
