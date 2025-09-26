3d39c711a0a8eb7fd88cb1b5ce1c6b012ff51af8
vb64 <vit.sar68@gmail.com>
2025-09-26 22:11:25 +0400

    step

---
+++ tests/test/test_csv/test_statistics/test_totals.py
@@ -0,0 +1,20 @@
+"""Tests totals.py file.
+
+make test T=test_csv/test_statistics/test_totals.py
+"""
+from . import TestStatistics
+
+
+class TestTotals(TestStatistics):
+    """File totals.py."""
+
+    def test_property_counter(self):
+        """Check Totals class."""
+        from pipeline_csv.csvfile.statistics.totals import Totals
+
+        totals = Totals()
+        assert not totals.markers
+        assert "Tubes:" in str(totals)
+
+        self.tube.length = 100
+        totals.add_data(self.tube)
