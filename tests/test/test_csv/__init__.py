faf57c5b1a281452bec8d13f65b5054b73f454cc
vb64 <vit.sar68@gmail.com>
2025-09-26 21:56:10 +0400

    tests

--- tests/test/test_csv/__init__.py
+++ tests/test/test_csv/__init__.py
@@ -5,4 +5,12 @@ from .. import TestIV
 class TestCsv(TestIV):
     """Base class for InspectionViewer csv tests."""
 
-    pass
+    def setUp(self):
+        """Init tube tests."""
+        TestIV.setUp(self)
+        from pipeline_csv.csvfile import Stream
+        from pipeline_csv.csvfile.tubes import Tube
+        from pipeline_csv.csvfile.row import Row
+
+        stream = Stream(diameter=700)
+        self.tube = Tube(Row.as_weld(10), stream, '1')
---
+++ tests/test/test_csv/test_statistics/__init__.py
@@ -0,0 +1,6 @@
+"""Root class for testing iv package."""
+from .. import TestCsv
+
+
+class TestStatistics(TestCsv):
+    """Base class for InspectionViewer csv statistics tests."""
