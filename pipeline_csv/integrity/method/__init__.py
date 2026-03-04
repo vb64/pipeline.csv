f79e7ea99a33db253b72111a182b32e96e3b7069
vb64 <vit.sar68@gmail.com>
2026-03-04 23:51:30 +0400

    Base Context class for integrity methods.

---
+++ pipeline_csv/integrity/method/__init__.py
@@ -0,0 +1,38 @@
+"""Danger calculation methods."""
+
+
+class Context:
+    """Base class for Context of the danger method."""
+
+    name = "not defined"
+
+    def __init__(self, defect, material, pressure, extended_data=None):
+        """Create new context instance."""
+        self.anomaly = defect
+        self.material = material
+        self.pressure = pressure
+        self.extended_data = extended_data
+        self.explain_text = []
+        self.is_explain = False
+
+    def __str__(self):
+        """Return as text."""
+        return "{}\nPipe {}\nDfct {}".format(
+          self.name,
+          str(self.anomaly.pipe),
+          str(self.anomaly)
+        )
+
+    @classmethod
+    def lang(cls, _lang_code):
+        """Load language dict for localize explain text."""
+        raise NotImplementedError("{}.lang".format(cls.__class__.__name__))
+
+    def explain(self):
+        """Return text with explanation for calculation."""
+        return ''.join(self.explain_text)
+
+    def add_explain(self, msg_list):
+        """Add messages from the list to explain array."""
+        if self.is_explain:
+            self.explain_text.extend(msg_list)
