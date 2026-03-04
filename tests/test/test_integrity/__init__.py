39139a852925ffc07dc261bc5c63721cde382048
Vitaly Bogomolov <vit.sar68@gmail.com>
2026-03-04 17:21:34 +0400

    step

---
+++ tests/test/test_integrity/__init__.py
@@ -0,0 +1,6 @@
+"""Root class for testing integrity package."""
+from .. import TestIV
+
+
+class TestIntegrity(TestIV):
+    """Base class for Integrity tests."""
---
+++ tests/test/test_integrity/test_i18n.py
@@ -0,0 +1,16 @@
+"""Tests i18n.py file.
+
+make test T=test_csv/test_i18n.py
+"""
+from . import TestIntegrity
+
+
+class TestI18n(TestIntegrity):
+    """File i18n.py."""
+
+    def test_load_po(self):
+        """Check load_po function."""
+        from pipeline_csv.integrity.i18n import load_po
+
+        data = load_po(self.fixture('ru.po'))
+        assert len(data) > 1
