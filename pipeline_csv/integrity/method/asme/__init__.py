14c44d0483b939b3cbb4e433e965988276344e2c
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2026-03-05 21:45:27 +0400

    Base class for method Asme. (#109)

---
+++ pipeline_csv/integrity/method/asme/__init__.py
@@ -0,0 +1,33 @@
+"""ASME B31G method for metal loss defects.
+
+Another libs:
+
+https://pypi.org/project/pyintegrity/
+https://github.com/novanumeric/WebIntegrity
+"""
+import os
+
+from .. import Context as ContextBase
+from ...i18n import load_po
+
+EXPL_ROUND = 3
+
+
+class Context(ContextBase):
+    """Context of the ASME B31G method."""
+
+    # ANSI/NACE SP0502-2010 (formerly RP0502) Appendix C: Postassessment: Corrosion Rate Estimation C3.2
+    # https://github.com/vb64/pipeline.integrity/blob/main/docs/SOP_Pipeline_External_Corrosion.pdf
+    corrosion_rate = 0.4  # mm/year
+    design_factor = 1.0
+
+    def __init__(self, defect, material, pressure):
+        """ASME B31G context."""
+        super().__init__(defect, material, pressure)
+        self.safe_pressure = None
+
+    @classmethod
+    def lang(cls, lang_code):
+        """Load language dict for localize explain text."""
+        name = os.path.join(os.path.dirname(__file__), 'locale', lang_code, 'LC_MESSAGES', 'messages.po')
+        return load_po(name)
